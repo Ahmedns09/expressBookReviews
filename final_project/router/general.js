09834244334fe553a9f916b98209ddff89a1d907
Ahmedns09 <anskhzyan@gmail.com>
2025-10-08 14:54:20 -0400

    modified the routes using promise callbacks and Async/Await for asynchronous operation

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,43 +25,125 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
     //Write your code here
+    //Asynchronous operation using Promise
 
-    return res.status(200).send(JSON.stringify(books, null, 4));
+    const myPromise = new Promise((resolve, reject) => {
+        if (books) {
+            resolve(books);
+        } else {
+            reject("Books not found!");
+        }
+    });
+
+    myPromise.then((data) => {
+        return res.status(200).send(JSON.stringify(books, null, 4));
+    })
+        .catch((err) => {
+            return res.status(500).json({ message: err });
+        });
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
     //Write your code here
-
+    //Asynchronous operation using Promise
     let isbn = req.params.isbn;
-    return res.status(200).json(books[isbn]);
+
+    const myPromise1 = new Promise((resolve, reject) => {
+        if (books) {
+            resolve(books);
+        } else {
+            reject("No books found!")
+        }
+    });
+
+    const myPromise2 = new Promise((resolve, reject) => {
+        if (books.hasOwnProperty(isbn)) {
+            resolve(true);
+        } else {
+            reject("Invalid ISBN");
+        }
+    });
+
+    myPromise1.then((data) => {
+        myPromise2.then((response) => {
+            return res.status(200).json(data[isbn]);
+        })
+            .catch((err2) => {
+                return res.status(400).json({ message: err2 });
+            });
+    })
+        .catch((err1) => {
+            return res.status(500).json({ message: err1 });
+        });
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     //Write your code here
-
-    let author = req.params.author;
-    for (let isbn in books){
-        if (books[isbn]["author"] === author){
-            return res.json(books[isbn]);
+    //Asynchronous operation using Async/Await
+    async function fetchBook(data) {
+        if (data) {
+            return data;
+        } else {
+            throw new Error("No books found!")
         }
     }
-    return res.status(400).json({ message: "Invalid author!" });
+
+    try {
+        let author = req.params.author;
+        const data = await fetchBook(books);
+        let booksByauthor = [];
+
+        for (let isbn in data) {
+            if (data[isbn]["author"] === author) {
+                booksByauthor.push(data[isbn]);
+
+            }
+        }
+        if (booksByauthor.length === 0) {
+            return res.status(400).json({ message: "Invalid Author!" });
+        }
+
+        return res.status(200).json(booksByauthor);
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     //Write your code here
-
-    let title = req.params.title;
-    for (let isbn in books){
-        if (books[isbn]["title"] === title){
-            return res.json(books[isbn]);
+    //Asynchronous operation using Async/Await
+    async function fetchBook(data) {
+        if (data) {
+            return data;
+        } else {
+            throw new Error("No books found!");
         }
     }
-    return res.status(400).json({ message: "Invalid title!" });
-    
+
+    try {
+        let title = req.params.title;
+        const data = await fetchBook(books);
+        let booksByTitle = [];
+
+        for (let isbn in data) {
+            if (data[isbn]["title"] === title) {
+                booksByTitle.push(data[isbn]);
+            }
+        }
+
+        if (booksByTitle.length === 0) {
+            return res.status(400).json({ message: "Invalid title!" });
+        }
+
+        return res.status(200).json(booksByTitle);
+    }
+    catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
 });
 
 //  Get book review
@@ -69,7 +151,7 @@ public_users.get('/review/:isbn', function (req, res) {
     //Write your code here
 
     let isbn = req.params.isbn;
-    if(Object.keys(books).includes(isbn)){
+    if (Object.keys(books).includes(isbn)) {
         res.json(books[isbn]["reviews"]);
     }
     return res.status(400).json({ message: "Invalid ISBN" });
